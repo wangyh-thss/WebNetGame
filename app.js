14b56055c0c4502643dd25218a9cafe184be1f04
tarma <tarma_fu@hotmail.com>
2015-05-27 11:18:29 +0800

    Add http server

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tarma on 15年5月27日.
+ */
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname));
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
