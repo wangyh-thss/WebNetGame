84bd261a67a789bbfc8c9553f6d9e480d54d6541
tarma <tarma_fu@hotmail.com>
2015-05-28 15:01:22 +0800

    add room control

--- app.js
+++ app.js
@@ -7,153 +7,6 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var Maze = require('./Maze.js');
 
-var maze = new Maze({
-    width: 10,
-    height: 10,
-    perfect: true,
-    braid: false,
-    checkOver: false,
-
-    onInit: function() {
-        this.checkOver = false;
-        this.checkCount = {};
-        // this.traceInfo = {};
-        this.foundEndNode = false;
-    },
-    getNeighbor: function() {
-        // if (this.currentDirCount < 6 && this.neighbors[this.currentDir]) {
-        //     return this.neighbors[this.currentDir];
-        // }
-        var list = this.neighbors.list;
-        var n = list[list.length * Math.random() >> 0];
-        return n;
-    },
-
-    isValid: function(nearNode, node, dir) {
-        if (!nearNode || nearNode.value === null) {
-            return false;
-        }
-        if (nearNode.value === 0) {
-            return true;
-        }
-        if (this.perfect || this.braid) {
-            return false;
-        }
-        var c = nearNode.x,
-            r = nearNode.y;
-        // 用于生成一种非Perfect迷宫
-        this.checkCount[c + "-" + r] = this.checkCount[c + "-" + r] || 0;
-        var count = ++this.checkCount[c + "-" + r];
-        return Math.random() < 0.3 && count < 3;
-    },
-    beforeBacktrace: function() {
-        // if (!this.braid || Math.random() < 0.5) {
-        if (!this.braid) {
-            return;
-        }
-        var n = [];
-        var node = this.currentNode;
-        var c = node.x;
-        var r = node.y;
-        var nearNode, dir, op;
-
-        var first = null;
-        var currentDir = this.currentDir;
-        var updateNear = function() {
-            op = Maze.Direction.opposite[dir];
-            if (nearNode && nearNode.value !== null && (nearNode.value & op) !== op) {
-                n.push([nearNode, dir]);
-                if (dir == currentDir) {
-                    first = [nearNode, dir];
-                }
-            }
-        };
-
-        dir = Maze.Direction.N;
-        nearNode = r > 0 ? this.grid[r - 1][c] : null;
-        updateNear();
-
-        if (!first) {
-            dir = Maze.Direction.E;
-            nearNode = this.grid[r][c + 1];
-            updateNear();
-        }
-
-        if (!first) {
-            dir = Maze.Direction.S;
-            nearNode = r < this.height - 1 ? this.grid[r + 1][c] : null;
-            updateNear();
-        }
-
-        if (!first) {
-            dir = Maze.Direction.W;
-            nearNode = this.grid[r][c - 1];
-            updateNear();
-        }
-
-        n = first || n[n.length * Math.random() >> 0];
-        this.moveTo(n[0], n[1]);
-    },
-    updateCurrent: function() {
-        if (this.braid) {
-            return;
-        }
-        // 每步有 10% 的概率 进行回溯
-        if (Math.random() <= 0.10) {
-            this.backtrace();
-        }
-    },
-
-    getTraceIndex: function() {
-        var len = this.trace.length;
-
-        if (this.braid) {
-            return len - 1;
-        }
-
-        // 按一定的概率随机选择回溯策略
-        var r = Math.random();
-        var idx = 0;
-        if (r < 0.5) {
-            idx = len - 1;
-        } else if (r < 0.7) {
-            idx = len >> 1;
-        } else if (r < 0.8) {
-            idx = len * Math.random() >> 0;
-        }
-        return idx;
-    },
-
-    afterGenrate: function() {
-        if (this.braid && this.getRoadCount(this.startNode)<2) {
-            this.currentDirCount = 1000;
-            this.setCurrent(this.startNode);
-            this.nextStep();
-        }
-    },
-
-    isOver: function() {
-        if (!this.checkOver) {
-            return false;
-        }
-        if (this.currentNode == this.endNode) {
-            this.foundEndNode = true;
-        }
-        // 当探索到迷宫终点, 且探索了至少一半的区域时,终止迷宫的生成
-        if (this.foundEndNode && this.stepCount >= this.size / 2) {
-            return true;
-        }
-        return false;
-    }
-});
-
-maze.init();
-maze.startNode = maze.getRandomNode();
-do {
-    maze.endNode = maze.getRandomNode();
-} while (maze.startNode == maze.endNode);
-maze.generate();
-
 server.listen(3000);
 
 app.get('/', function (req, res) {
@@ -162,39 +15,205 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname));
 
-var player_count = 0;
+var player_count = {};
+var maze = {};
 
 io.on('connection', function (socket) {
-    if (player_count < 2) {
-        player_count++;
-        io.emit('init', {
-            'maze': maze,
-            'player': player_count
-        });
-    } else {
-        return;
-    }
-    socket.on('fire', function(id) {
-        socket.broadcast.emit('fire', id);
-    });
-
-    socket.on('run', function(data) {
-        socket.broadcast.emit('run', data);
-    });
-
-    socket.on('rotate', function(data) {
-        socket.broadcast.emit('rotate', data);
-    });
-
-    socket.on('stopRun', function(data) {
-        socket.broadcast.emit('stopRun', data);
-    });
-
-    socket.on('stopRotate', function(data) {
-        socket.broadcast.emit('stopRotate', data);
-    });
-
-    socket.on('disconnect', function () {
-        player_count--;
+    socket.on('room', function(data) {
+        if (!player_count[data.roomName] || player_count[data.roomName] == 0) {
+            onNewNameSpace(data.roomName);
+        }
+        socket.emit('join room', data.roomName);
     });
 });
+
+function onNewNameSpace(namespace) {
+    player_count[namespace] = 0;
+    maze[namespace] = createNewMaze();
+    io.of('/' + namespace).on('connection', function(socket) {
+        if (player_count[namespace] < 2) {
+            player_count[namespace]++;
+            socket.emit('init', {
+                'maze': maze[namespace],
+                'player': player_count[namespace]
+            });
+        } else {
+            return;
+        }
+
+        socket.on('fire', function(id) {
+            socket.broadcast.emit('fire', id);
+        });
+
+        socket.on('run', function(data) {
+            socket.broadcast.emit('run', data);
+        });
+
+        socket.on('rotate', function(data) {
+            socket.broadcast.emit('rotate', data);
+        });
+
+        socket.on('stopRun', function(data) {
+            socket.broadcast.emit('stopRun', data);
+        });
+
+        socket.on('stopRotate', function(data) {
+            socket.broadcast.emit('stopRotate', data);
+        });
+
+        socket.on('disconnect', function () {
+            player_count[namespace]--;
+        });
+    });
+}
+
+function createNewMaze() {
+    var maze = new Maze({
+        width: 10,
+        height: 10,
+        perfect: true,
+        braid: false,
+        checkOver: false,
+
+        onInit: function() {
+            this.checkOver = false;
+            this.checkCount = {};
+            // this.traceInfo = {};
+            this.foundEndNode = false;
+        },
+        getNeighbor: function() {
+            // if (this.currentDirCount < 6 && this.neighbors[this.currentDir]) {
+            //     return this.neighbors[this.currentDir];
+            // }
+            var list = this.neighbors.list;
+            var n = list[list.length * Math.random() >> 0];
+            return n;
+        },
+
+        isValid: function(nearNode, node, dir) {
+            if (!nearNode || nearNode.value === null) {
+                return false;
+            }
+            if (nearNode.value === 0) {
+                return true;
+            }
+            if (this.perfect || this.braid) {
+                return false;
+            }
+            var c = nearNode.x,
+                r = nearNode.y;
+            // 用于生成一种非Perfect迷宫
+            this.checkCount[c + "-" + r] = this.checkCount[c + "-" + r] || 0;
+            var count = ++this.checkCount[c + "-" + r];
+            return Math.random() < 0.3 && count < 3;
+        },
+        beforeBacktrace: function() {
+            // if (!this.braid || Math.random() < 0.5) {
+            if (!this.braid) {
+                return;
+            }
+            var n = [];
+            var node = this.currentNode;
+            var c = node.x;
+            var r = node.y;
+            var nearNode, dir, op;
+
+            var first = null;
+            var currentDir = this.currentDir;
+            var updateNear = function() {
+                op = Maze.Direction.opposite[dir];
+                if (nearNode && nearNode.value !== null && (nearNode.value & op) !== op) {
+                    n.push([nearNode, dir]);
+                    if (dir == currentDir) {
+                        first = [nearNode, dir];
+                    }
+                }
+            };
+
+            dir = Maze.Direction.N;
+            nearNode = r > 0 ? this.grid[r - 1][c] : null;
+            updateNear();
+
+            if (!first) {
+                dir = Maze.Direction.E;
+                nearNode = this.grid[r][c + 1];
+                updateNear();
+            }
+
+            if (!first) {
+                dir = Maze.Direction.S;
+                nearNode = r < this.height - 1 ? this.grid[r + 1][c] : null;
+                updateNear();
+            }
+
+            if (!first) {
+                dir = Maze.Direction.W;
+                nearNode = this.grid[r][c - 1];
+                updateNear();
+            }
+
+            n = first || n[n.length * Math.random() >> 0];
+            this.moveTo(n[0], n[1]);
+        },
+        updateCurrent: function() {
+            if (this.braid) {
+                return;
+            }
+            // 每步有 10% 的概率 进行回溯
+            if (Math.random() <= 0.10) {
+                this.backtrace();
+            }
+        },
+
+        getTraceIndex: function() {
+            var len = this.trace.length;
+
+            if (this.braid) {
+                return len - 1;
+            }
+
+            // 按一定的概率随机选择回溯策略
+            var r = Math.random();
+            var idx = 0;
+            if (r < 0.5) {
+                idx = len - 1;
+            } else if (r < 0.7) {
+                idx = len >> 1;
+            } else if (r < 0.8) {
+                idx = len * Math.random() >> 0;
+            }
+            return idx;
+        },
+
+        afterGenrate: function() {
+            if (this.braid && this.getRoadCount(this.startNode)<2) {
+                this.currentDirCount = 1000;
+                this.setCurrent(this.startNode);
+                this.nextStep();
+            }
+        },
+
+        isOver: function() {
+            if (!this.checkOver) {
+                return false;
+            }
+            if (this.currentNode == this.endNode) {
+                this.foundEndNode = true;
+            }
+            // 当探索到迷宫终点, 且探索了至少一半的区域时,终止迷宫的生成
+            if (this.foundEndNode && this.stepCount >= this.size / 2) {
+                return true;
+            }
+            return false;
+        }
+    });
+
+    maze.init();
+    maze.startNode = maze.getRandomNode();
+    do {
+        maze.endNode = maze.getRandomNode();
+    } while (maze.startNode == maze.endNode);
+    maze.generate();
+
+    return maze;
+}
